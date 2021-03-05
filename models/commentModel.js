5233a3c3674d2dbaa2cfa839e879ea96e01d00ea
igaue-takahhiko <hasewo.2610@gmail.com>
2021-03-05 13:17:32 +0900

    implemet postModel API

---
+++ models/commentModel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema(
+  {
+    
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("comment", commentSchema);
