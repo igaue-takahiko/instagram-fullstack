4347a355aee1159d9232e0a3bd0f68dd4edc51e2
igaue-takahhiko <hasewo.2610@gmail.com>
2021-03-13 20:06:28 +0900

    implement likePost API

--- controllers/postCtrl.js
+++ controllers/postCtrl.js
@@ -58,6 +58,41 @@ const postCtrl = {
       return res.status(500).json({ msg: error.message });
     }
   },
+  likePost: async (req, res) => {
+    try {
+      const post = await Posts.find({
+        _id: req.params.id,
+        likes: req.user._id,
+      });
+      if (post.length > 0) {
+        return res.status(400).json({ msg: "You liked this post." });
+      }
+
+      await Posts.findOneAndUpdate(
+        { _id: req.params.id },
+        { $push: { likes: req.user._id } },
+        { new: true }
+      );
+
+      res.json({ msg: "Liked Post!" });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+  unLikePost: async (req, res) => {
+    try {
+
+      await Posts.findOneAndUpdate(
+        { _id: req.params.id },
+        { $pull: { likes: req.user._id } },
+        { new: true }
+      );
+
+      res.json({ msg: "UnLiked Post!" });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
 };
 
 module.exports = postCtrl;
